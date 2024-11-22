6d57a4efa2418b931a3d6c1d289737e3c91637c3
SevenPik <sevenpik1234@gmail.com>
2024-11-23 00:41:47 +0100

    movement lock

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -2,17 +2,50 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private Transform target;
-    public float speed;
+    private Transform target; // Cel, którym będzie gracz
+    public float speed;       // Prędkość poruszania
+    public bool useTriggers;  // Ustaw na true, jeśli chcesz używać triggerów zamiast kolizji fizycznych
+
     void Start()
     {
+        // Znajdź obiekt gracza za pomocą tagu
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (target != null)
+        {
+            // Poruszaj się tylko w osi Y w kierunku celu
+            Vector2 newPosition = new Vector2(transform.position.x, 
+                Mathf.MoveTowards(transform.position.y, target.position.y, speed * Time.deltaTime));
+            transform.position = newPosition;
+        }
     }
+
+    // Funkcja wykrywania kolizji (dla Collider 2D)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!useTriggers)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("Zderzenie z graczem!");
+                // Możesz tu dodać efekt np. zadanie obrażeń
+            }
+        }
+    }
+
+    // Funkcja wykrywania wejścia w trigger (dla Trigger 2D)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (useTriggers)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                Debug.Log("Gracz wszedł w trigger!");
+                // Dodaj zachowanie, jeśli obiekt dotknął gracza
+            }
+     
+        }}
 }
