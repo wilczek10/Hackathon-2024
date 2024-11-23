dd59b716853868ce78ffb7fb51cb7e124c5e31ee
SevenPik <sevenpik1234@gmail.com>
2024-11-23 04:12:13 +0100

    asd

--- Assets/zycie.cs
+++ Assets/zycie.cs
@@ -1,29 +1,24 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    private int currentHealth;
-
+    public float maxHealth = 100;
+    public float currentHealth;
+    [SerializeField] private Image healthBarFill;
     void Start()
     {
         currentHealth = maxHealth;
     }
 
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        Debug.Log("Player Health: " + currentHealth);
 
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
+    public void UpdateHealth(float amount) {
+        currentHealth +=amount;
+        UpdateHealthBar();
+    }
+    private void UpdateHealthBar() {
+        float targetFillAmount = currentHealth / maxHealth;
+        healthBarFill.fillAmount = targetFillAmount;
     }
 
-    void Die()
-    {
-        Debug.Log("Player is dead!");
-        // Obs³uga œmierci gracza (respawn, koniec gry itp.)
-    }
+
 }
